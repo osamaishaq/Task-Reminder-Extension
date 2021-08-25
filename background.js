cd34db6c16912078f0df940d148ac56ed2bf2bfa
osama <osama.ishaq10@gmail.com>
2021-08-25 21:25:38 +0500

    Tomato Timer Extention Completed

--- background.js
+++ background.js
@@ -1,34 +1,35 @@
 chrome.alarms.create("pomodoroTimer", {
-    periodInMinutes: 1/60
-})
+  periodInMinutes: 1 / 60,
+});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if(alarm.name == "pomodoroTimer"){
-        chrome.storage.local.get(["timer", "isRunning"],(res) => {
-            if(res.isRunning){
-                let timer = res.timer + 1
-                let isRunning = true
-                if(timer === 60 * 25){
-                    this.registration.showNotification("Timer is Up",{
-                        body: '25 minutes has passed!',
-                        icon: "icon.png"
-                    })
-                    timer = 0
-                    isRunning = false
-                }
-                //console.log("timer Value ==> ", timer)
-                chrome.storage.local.set({
-                    timer,
-                    isRunning,
-                })
-            }
-        })
-    }
-})
+  if (alarm.name == "pomodoroTimer") {
+    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer === 60 * res.timeOption) {
+          this.registration.showNotification("Timer is Up", {
+            body: `${res.timeOption} minutes has passed!`,
+            icon: "icon.png",
+          });
+          timer = 0;
+          isRunning = false;
+        }
+        //console.log("timer Value ==> ", timer)
+        chrome.storage.local.set({
+          timer,
+          isRunning,
+        });
+      }
+    });
+  }
+});
 
-chrome.storage.local.get(["timer", "isRunning"], (res) => {
-    chrome.storage.local.set({
-        timer: "timer" in res ? res.timer : 0,
-        isRunning: "isRunning" in res ? res.isRunning : 0
-    })
-})
+chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    isRunning: "isRunning" in res ? res.isRunning : 0,
+    timeOption: "timeOption" in res ? res.timeOption : 25,
+  });
+});
